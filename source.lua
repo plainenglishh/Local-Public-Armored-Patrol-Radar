2d8cf3ff9d08608c2b4314adf130f821f8200698
plainenglishh <20248750+plainenglishh@users.noreply.github.com>
2022-04-10 01:06:02 +0100

    Many improvements

--- source.lua
+++ source.lua
@@ -6,10 +6,24 @@ local HttpService = game:GetService("HttpService");
 local function CreatePlayerInfo(Player)
 	local X = -1;
 	local Y = -1;
-	
+	local Alt = "0%";
+
+	local VX = 0;
+	local VY = 0;
+
+	local Health = 0;
+
 	if Player.Character and Player.Character:FindFirstChild("HumanoidRootPart") then
 		X = Player.Character.HumanoidRootPart.Position.X;
 		Y = Player.Character.HumanoidRootPart.Position.Z;
+		Alt = Player.Character.HumanoidRootPart.Position.Y;
+
+		VX = Player.Character.HumanoidRootPart.Velocity.X;
+		VY = Player.Character.HumanoidRootPart.Velocity.Z;
+	end
+
+	if Player.Character and Player.Character:FindFirstChild("Humanoid") then
+		Health = math.round((Player.Character.Humanoid.Health / Player.Character.Humanoid.MaxHealth) * 100).."%";
 	end
 
 	local TeamColor = Player.TeamColor or BrickColor.Black();
@@ -18,11 +32,18 @@ local function CreatePlayerInfo(Player)
 	return {
 		Name = Player.Name,
 		Position = {
-			X = X,
-			Y = Y
+			X = math.round(X),
+			Y = math.round(Y),
+			Alt = math.round(Alt)
 		},
-		TeamColor = string.format("rgb(%s,%s,%s)", TeamColor.R * 255, TeamColor.G * 255, TeamColor.B * 255),
-		LocalPlayer = Player == Players.LocalPlayer
+		Velocity = {
+			X = math.round(VX),
+			Y = math.round(VY),
+		},
+		TeamColor = string.format("rgb(%s,%s,%s)", math.round(TeamColor.R * 255), math.round(TeamColor.G * 255), math.round(TeamColor.B * 255)),
+		LocalPlayer = Player == Players.LocalPlayer,
+		TeamMate = Player.Team == Players.LocalPlayer.Team,
+		Health = Health
 	}
 end
 
@@ -31,7 +52,7 @@ local function LocalData()
 	local Team = "Neutral";
 
 	if Players.LocalPlayer.Character and Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
-		Health = ((Players.LocalPlayer.Character.Humanoid.Health / Players.LocalPlayer.Character.Humanoid.MaxHealth) * 100).."%";
+		Health = math.round((Players.LocalPlayer.Character.Humanoid.Health / Players.LocalPlayer.Character.Humanoid.MaxHealth) * 100).."%";
 	end
 
 	if Players.LocalPlayer.Team then
@@ -45,12 +66,29 @@ local function LocalData()
 	}
 end
 
+local function ListBolts()
+	if game.GameId ~= 539067902 then return {} end
+
+	local Bolts = {};
+	for _, BoltInstance in pairs(game:GetService("Workspace").ActiveBolts:GetChildren()) do
+		if BoltInstance.Name == "C4" or BoltInstance.Name == "Dynamite" or BoltInstance.Name == "Grenade" then
+			table.insert(Bolts, {"Explosive", math.round(BoltInstance.Position.X), math.round(BoltInstance.Position.Z)});
+		elseif BoltInstance.Name == "Shell" or BoltInstance.Name == "Missile" then
+			table.insert(Bolts, {"Projectile", math.round(BoltInstance.Position.X), math.round(BoltInstance.Position.Z)});
+		elseif BoltInstance.Name == "Slug" then
+			table.insert(Bolts, {"MinorProjectile", math.round(BoltInstance.Position.X), math.round(BoltInstance.Position.Z)});
+		end
+	end
+	return Bolts;
+end
+
 while task.wait(0.1) do
 	local Data = {
 		PlayerLocations = {
 			
 		},
-		LocalData = LocalData()
+		LocalData = LocalData(),
+		Bolts = ListBolts()
 	};
 
 	for _, Player in ipairs(Players:GetPlayers()) do
